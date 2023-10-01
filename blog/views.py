a910eec44b684c3a992ac9193edf544af84ee14f
amin00879 <aminavatefi87@gmail.com>
2023-10-01 18:10:02 +0330

    f2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from .models import Post
+from django.contrib.auth.models import User
+def creat100(requests):
+    me = User.objects.get(username='admin')
+    p=Post.objects.create(author=me, title='fotbal_omid{i}', text='fotbal_omid baxt')
+    # p=Post()
+    # p.title='fotbal_omid'
+    # p.text='fotbal_omid baxt'
+    # p.author=
+    
+    p.publish()
+    return HttpRequest("Done")
 
 # Create your views here.
